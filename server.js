2d7667b7b15775888245a78bbe8469c66c8d3267
Maki Aboabida <maki-miko@hotmail.com>
2021-10-18 20:06:43 -0400

    Added: Create a login and signup page (#7)

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
+const path = require('path');
 const express = require('express');
-const controllers = require('./controllers');
-const path = require('path')
-const sequelize = require('./config/connection');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+const sess = {
+  secret: process.env.SECRET_WORD,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(controllers);
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
